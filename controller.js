bbabc0c571bdd8e0a15360b38decb3b74fe2bbb8
purwokang <purwo.com@gmail.com>
2020-12-18 15:11:18 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -2,7 +2,19 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function (req, res) {
     response.ok('Aplikasi REST API kita berjalan!', res)
+};
+
+// menampilkan data semua mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
